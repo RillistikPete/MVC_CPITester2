cbcca451929ee0c894d9b9728b0fc8902b1b4a8a
Gavin Forrest <petergforrest91@gmail.com>
2022-02-02 16:17:03 -0600

    Create CPIProject.cs

---
+++ CPIProject.cs
@@ -0,0 +1,37 @@
+namespace MVCTesterCPI2
+{
+    using System;
+    using System.Collections.Generic;
+    
+    public partial class CPIProject
+    {
+        public int ID { get; set; }
+        public int ProgramID { get; set; }
+        public string DODDeptSvcAgncy { get; set; }
+        public string SyscomOrgLab { get; set; }
+        public string ProjectName { get; set; }
+        public string VersionIncrementSpiral { get; set; }
+        public string ProgramOffice { get; set; }
+        public string ProgramOfficePOC { get; set; }
+        public string ACAT { get; set; }
+        public string EffortType { get; set; }
+        public bool ScientificResearchSurveyA { get; set; }
+        public bool ScientificResearchSurveyB { get; set; }
+        public bool ScientificResearchSurveyC { get; set; }
+        public bool ScientificResearchSurveyD { get; set; }
+        public string ScientificResearchDefnA { get; set; }
+        public bool CPISurveyA { get; set; }
+        public bool CPISurveyA1 { get; set; }
+        public bool CPISurveyA2 { get; set; }
+        public bool CPISurveyB { get; set; }
+        public bool CPISurveyC { get; set; }
+        public bool CPISurveyD { get; set; }
+        public bool CPISurveyE { get; set; }
+        public bool CPISurveyF { get; set; }
+        public bool CPISurveyG { get; set; }
+        public bool CPISurveyH { get; set; }
+        public string LastEditBy { get; set; }
+        public string LastEditDate { get; set; }
+        public string CreatedDate { get; set; }
+    }
+}
